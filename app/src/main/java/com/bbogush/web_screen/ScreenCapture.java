54d80473e3d9c4b403ba034a311ab33b7edf4740
Bogdan Bogush <bogdan.s.bogush@gmail.com>
2020-05-22 19:26:39 +0300

    Refactored screen capture class

--- app/src/main/java/com/bbogush/web_screen/ScreenCapture.java
+++ app/src/main/java/com/bbogush/web_screen/ScreenCapture.java
@@ -1,72 +1,31 @@
 package com.bbogush.web_screen;
 
-import android.app.Service;
-import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.graphics.ImageFormat;
 import android.graphics.PixelFormat;
-import android.graphics.Point;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.VirtualDisplay;
 import android.media.Image;
 import android.media.ImageReader;
 import android.media.projection.MediaProjection;
-import android.media.projection.MediaProjectionManager;
-import android.os.Build;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Display;
-import android.view.WindowManager;
-
-import androidx.annotation.RequiresApi;
-
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ScreenCapture {
-    private Display display;
-    private MediaProjection mediaProjection;
     private ImageReader imageReader;
     Bitmap bitmap = null;
     public AtomicBoolean bitmapDataLock = new AtomicBoolean(false);
     VirtualDisplay virtualDisplay;
-    Handler handler = new Handler(Looper.getMainLooper());
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public void start(MediaProjection mediaProjection) {
-        //Point screenSize = new Point();
-        //DisplayMetrics screenMetrics = new DisplayMetrics();
-        //Display display = getWindowManager().getDefaultDisplay();
-
-        // start capture handling thread
-
-//        new Thread()
-//        {
-//            @Override
-//            public void run()
-//            {
-//                Looper.prepare();
-//                handler = new Handler(Looper.getMainLooper());
-//                Looper.loop();
-//            }
-//        }.start();
-
-
         DisplayMetrics screenMetrics = Resources.getSystem().getDisplayMetrics();
 
-
-        //display.getRealSize(screenSize);
-        //display.getMetrics(screenMetrics);
-
-        Log.d("ScreenCapture", "w=" + screenMetrics.widthPixels + " h=" + screenMetrics.heightPixels);
-
         try {
-            imageReader = ImageReader.newInstance(838, 1244,
-                    PixelFormat.RGBA_8888, 1);
+            imageReader = ImageReader.newInstance(screenMetrics.widthPixels,
+                    screenMetrics.heightPixels, PixelFormat.RGBA_8888, 2);
         } catch (Exception e) {
             e.printStackTrace();
+            return;
         }
 
         imageReader.setOnImageAvailableListener(
@@ -82,24 +41,23 @@ public class ScreenCapture {
                 }, null);
 
         try {
-            virtualDisplay = mediaProjection.createVirtualDisplay(
-                    "ScreenStreamVirtualDisplay", 838, 1244,
-                    440, DisplayManager.VIRTUAL_DISPLAY_FLAG_PRESENTATION,
-                    imageReader.getSurface(), null, null);
+            virtualDisplay = mediaProjection.createVirtualDisplay("VirtualDisplay",
+                    screenMetrics.widthPixels, screenMetrics.heightPixels, screenMetrics.densityDpi,
+                    DisplayManager.VIRTUAL_DISPLAY_FLAG_PRESENTATION, imageReader.getSurface(),
+                    null, null);
         } catch (Exception e) {
             e.printStackTrace();
+            return;
         }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void stop() {
-        virtualDisplay.release();
         imageReader.close();
+        virtualDisplay.release();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     private void processScreenImage(Image image) {
-        Image.Plane [] planes = image.getPlanes();
+        Image.Plane[] planes = image.getPlanes();
         synchronized (bitmapDataLock) {
             bitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(),
                     Bitmap.Config.ARGB_8888);
