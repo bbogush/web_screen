0b43403ae3ba88253aced32104bb63ce4bb48b1e
Bogdan Bogush <bogdan.s.bogush@gmail.com>
2020-05-23 17:15:27 +0300

    Fixed creation of screan image

--- app/src/main/java/com/bbogush/web_screen/ScreenCapture.java
+++ app/src/main/java/com/bbogush/web_screen/ScreenCapture.java
@@ -9,6 +9,9 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.util.DisplayMetrics;
+import android.util.Log;
+
+import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ScreenCapture {
@@ -16,9 +19,10 @@ public class ScreenCapture {
     Bitmap bitmap = null;
     public AtomicBoolean bitmapDataLock = new AtomicBoolean(false);
     VirtualDisplay virtualDisplay;
+    DisplayMetrics screenMetrics;
 
     public void start(MediaProjection mediaProjection) {
-        DisplayMetrics screenMetrics = Resources.getSystem().getDisplayMetrics();
+        screenMetrics = Resources.getSystem().getDisplayMetrics();
 
         try {
             imageReader = ImageReader.newInstance(screenMetrics.widthPixels,
@@ -58,10 +62,21 @@ public class ScreenCapture {
 
     private void processScreenImage(Image image) {
         Image.Plane[] planes = image.getPlanes();
+        ByteBuffer buffer = planes[0].getBuffer();
+        int width = planes[0].getRowStride() / planes[0].getPixelStride();
+
         synchronized (bitmapDataLock) {
-            bitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(),
-                    Bitmap.Config.ARGB_8888);
-            bitmap.copyPixelsFromBuffer(planes[0].getBuffer());
+            if (width > image.getWidth()) {
+                Bitmap tempBitmap = Bitmap.createBitmap(width, screenMetrics.heightPixels,
+                        Bitmap.Config.ARGB_8888);
+                tempBitmap.copyPixelsFromBuffer(buffer);
+                bitmap = Bitmap.createBitmap(tempBitmap, 0, 0, image.getWidth(),
+                        image.getHeight());
+            } else {
+                bitmap = Bitmap.createBitmap(image.getWidth(), image.getHeight(),
+                        Bitmap.Config.ARGB_8888);
+                bitmap.copyPixelsFromBuffer(planes[0].getBuffer());
+            }
         }
     }
 
