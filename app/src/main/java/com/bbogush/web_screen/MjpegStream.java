68272b3d0cdddcea94af6b6cc51607914b578fde
Bogdan Bogush <bogdan.s.bogush@gmail.com>
2020-06-21 22:13:14 +0300

    Avoid temporary buffer for jpeg packet

--- app/src/main/java/com/bbogush/web_screen/MjpegStream.java
+++ app/src/main/java/com/bbogush/web_screen/MjpegStream.java
@@ -22,17 +22,23 @@ public class MjpegStream extends InputStream {
             boundaryLine.getBytes(StandardCharsets.US_ASCII);
     private static final byte [] contentTypeByteArray =
             contentType.getBytes(StandardCharsets.US_ASCII);
+    private byte [] contentLengthStringByteArray;
+    private byte [] imageByteArray;
 
     private ScreenCapture screenCapture;
     private ByteArrayOutputStream imageStream = new ByteArrayOutputStream();
-    private ByteArrayOutputStream dataStream = new ByteArrayOutputStream();
-    private byte [] data;
-    private int dataSize;
 
-    private boolean isFirstBoundary = true;
-    private int pos = 0;
+    private int sPos = 0, dPos = 0;
+    private boolean skipWait;
 
-    private Object syncToken = new Object();
+    private static final int STATE_FIRST_BOUNDARY = 0;
+    private static final int STATE_CONTENT_TYPE = 1;
+    private static final int STATE_CONTENT_LENGTH = 2;
+    private static final int STATE_IMAGE = 3;
+    private static final int STATE_TRAIL_BOUNDARY = 4;
+    private int state = STATE_FIRST_BOUNDARY;
+
+    private final Object syncToken = new Object();
     private OnBitmapAvailableListener bitmapListener = new OnBitmapAvailableListener();
 
     public MjpegStream(ScreenCapture screenCapture) {
@@ -46,8 +52,6 @@ public class MjpegStream extends InputStream {
     public void close() {
         Log.d(TAG, "Stream is closed");
         screenCapture.unregisterOnBitmapAvailableListener(bitmapListener);
-        isFirstBoundary = true;
-        pos = 0;
     }
 
     private class OnBitmapAvailableListener implements ScreenCapture.OnBitmapAvailableListener {
@@ -72,46 +76,148 @@ public class MjpegStream extends InputStream {
         throw new UnsupportedOperationException("read() method is not implemented");
     }
 
-    private void createContent() {
-        dataStream.reset();
-        if (isFirstBoundary) {
-            dataStream.write(boundaryLineByteArray, 0, boundaryLineByteArray.length);
-            isFirstBoundary = false;
-        }
-        dataStream.write(contentTypeByteArray, 0, contentTypeByteArray.length);
+    private void initContentLength() {
         String contentLengthString = String.format(Locale.US, contentLength, imageStream.size());
-        byte [] contentLengthStringByteArray =
-                contentLengthString.getBytes(StandardCharsets.US_ASCII);
-        dataStream.write(contentLengthStringByteArray, 0, contentLengthStringByteArray.length);
-        dataStream.write(imageStream.toByteArray(), 0, imageStream.size());
-        dataStream.write(boundaryLineByteArray, 0, boundaryLineByteArray.length);
-        data = dataStream.toByteArray();
-        dataSize = dataStream.size();
+        contentLengthStringByteArray = contentLengthString.getBytes(StandardCharsets.US_ASCII);
+    }
+
+    //TODO avoid toByteArray
+    private void initImage() {
+        imageByteArray = imageStream.toByteArray();
     }
 
     @Override
     public int read(byte[] buffer, int offset, int length) {
-        int copy = 0;
+        int copy = 0, copied = 0;
 
-        if (pos == 0) {
-            synchronized(syncToken) {
-                try {
-                    syncToken.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    return 0;
+        switch (state) {
+            case STATE_FIRST_BOUNDARY:
+                synchronized (syncToken) {
+                    try {
+                        syncToken.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return 0;
+                    }
                 }
+                copy = Math.min(length, boundaryLineByteArray.length - sPos);
+                System.arraycopy(boundaryLineByteArray, sPos, buffer, offset + dPos, copy);
+                sPos += copy;
+                dPos += copy;
+                copied += copy;
 
-                createContent();
-            }
+                length -= copy;
+                if (length > 0) {
+                    sPos = 0;
+                    skipWait = true;
+                    state = STATE_CONTENT_TYPE;
+                    // fall through
+                } else if (sPos == boundaryLineByteArray.length) {
+                    sPos = 0;
+                    dPos = 0;
+                    skipWait = true;
+                    state = STATE_CONTENT_TYPE;
+                    break;
+                } else {
+                    dPos = 0;
+                    break;
+                }
+            case STATE_CONTENT_TYPE:
+                synchronized (syncToken) {
+                    try {
+                        if (!skipWait)
+                            syncToken.wait();
+                        else
+                            skipWait = false;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        return 0;
+                    }
+                }
+                copy = Math.min(length, contentTypeByteArray.length - sPos);
+                System.arraycopy(contentTypeByteArray, sPos, buffer, offset + dPos, copy);
+                sPos += copy;
+                dPos += copy;
+                copied += copy;
+
+                length -= copy;
+                if (length > 0) {
+                    sPos = 0;
+                    state = STATE_CONTENT_LENGTH;
+                    initContentLength();
+                    // fall through
+                } else if (sPos == contentTypeByteArray.length) {
+                    sPos = 0;
+                    dPos = 0;
+                    state = STATE_CONTENT_LENGTH;
+                    initContentLength();
+                    break;
+                } else {
+                    dPos = 0;
+                    break;
+                }
+            case STATE_CONTENT_LENGTH:
+                copy = Math.min(length, contentLengthStringByteArray.length - sPos);
+                System.arraycopy(contentLengthStringByteArray, sPos, buffer, offset + dPos, copy);
+                sPos += copy;
+                dPos += copy;
+                copied += copy;
+
+                length -= copy;
+                if (length > 0) {
+                    sPos = 0;
+                    state = STATE_IMAGE;
+                    initImage();
+                    // fall through
+                } else if (sPos == contentLengthStringByteArray.length) {
+                    sPos = 0;
+                    dPos = 0;
+                    state = STATE_IMAGE;
+                    initImage();
+                    break;
+                } else {
+                    dPos = 0;
+                    break;
+                }
+            case STATE_IMAGE:
+                copy = Math.min(length, imageByteArray.length - sPos);
+                System.arraycopy(imageByteArray, sPos, buffer, offset + dPos, copy);
+                sPos += copy;
+                dPos += copy;
+                copied += copy;
+
+                length -= copy;
+                if (length > 0) {
+                    sPos = 0;
+                    state = STATE_TRAIL_BOUNDARY;
+                    // fall through
+                } else if (sPos == imageByteArray.length) {
+                    sPos = 0;
+                    dPos = 0;
+                    state = STATE_TRAIL_BOUNDARY;
+                    break;
+                } else {
+                    dPos = 0;
+                    break;
+                }
+            case STATE_TRAIL_BOUNDARY:
+                copy = Math.min(length, boundaryLineByteArray.length - sPos);
+                System.arraycopy(boundaryLineByteArray, sPos, buffer, offset + dPos, copy);
+                sPos += copy;
+                dPos += copy;
+                copied += copy;
+
+                if (sPos == boundaryLineByteArray.length) {
+                    sPos = 0;
+                    dPos = 0;
+                    state = STATE_CONTENT_TYPE;
+                    break;
+                } else {
+                    dPos = 0;
+                    break;
+                }
         }
 
-        copy = Math.min(length, dataSize - pos);
-        System.arraycopy(data, pos, buffer, offset, copy);
-        pos += copy;
-        if(pos >= dataSize)
-            pos = 0;
-
-        return copy;
+        return copied;
     }
 }
